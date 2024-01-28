8330a2d5a72828a481a2d8b4fc093aad6ef62cd9
Michael <kylleris.67515.steam@gmail.com>
2024-01-28 15:17:06 +0300

    index.php has been maded

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<meta charset="utf-8"/>
+<title> PenzGTU student's site</title>
+</head>
+<body>
+<center>Я Лисицин М. Ю. из группы 22ИЭ1б</center>
+<div class="mydiv1">
+<?php
+echo "объем шара радиусом 43 равен:";
+echo (4/3)*3.14*(43*43*43);
+?>
+</div>
+</body>
+</html>
